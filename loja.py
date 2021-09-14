2694242c01b512cf5fe462291c00ad8e91bebd01
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-09-14 12:31:13 -0300

    inclusao da classe ProdutoFisico

--- loja.py
+++ loja.py
@@ -32,3 +32,35 @@ class Produto:
                 raise ValueError("'preco' deve ser maior que zero.")
         else:
             raise TypeError("'preco' somente valores numericos sao aceitos")
+
+    def calcular_preco_com_frete(self):
+        return self.preco
+
+
+class ProdutoFisico(Produto):
+    def __init__(self, nome, preco, peso):
+        super().__init__(nome, preco)
+        self.__peso = self.peso = peso
+
+    @property
+    def peso(self):
+        return self.__peso
+
+    @peso.setter
+    def peso(self, novo_peso):
+        if not isinstance(novo_peso, bool) and isinstance(novo_peso, int):
+            if novo_peso > 0:
+                self.__peso = novo_peso
+            else:
+                raise ValueError("'peso' deve ser maior que zero")
+        else:
+            raise TypeError("'peso' deve ser um valor inteiro")
+
+    def peso_em_kg(self):
+        return self.peso / 1000
+
+    def calcular_preco_com_frete(self):
+        valor_por_kg = 5.0
+        return self.preco + (self.peso_em_kg() * valor_por_kg)
+
+
--- teste_unitario.py
+++ teste_unitario.py
@@ -1,5 +1,26 @@
 import loja
 
-loja = loja.Produto('', 0.1)
-print(loja.nome)
-print(loja.preco)
+
+def testa_produto():
+    produto = loja.Produto('PC', 1.500)
+    print(produto.nome)
+    print(produto.preco)
+    print(produto.calcular_preco_com_frete())
+
+def testa_produto_fisico():
+    produto_fisico = loja.ProdutoFisico('PC', 1500, 2)
+    print(produto_fisico.nome)
+    print(produto_fisico.peso)
+    print(produto_fisico.preco)
+    print(produto_fisico.peso_em_kg())
+    print(produto_fisico.calcular_preco_com_frete())
+
+
+if __name__ == "__main__":
+    print("testando classe Loja")
+    testa_produto()
+    print("*" * 60)
+    print("testando classe ProdutoFisico")
+    testa_produto_fisico()
+    print("*" * 60)
+
